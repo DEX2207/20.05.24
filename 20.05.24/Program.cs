fe4e75d67bd047511ea97bfd7f9e71a886394609
DEX2207 <123413987+DEX2207@users.noreply.github.com>
2024-05-24 11:19:45 +0300

    Add files via upload

--- 20.05.24/Program.cs
+++ 20.05.24/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace _20._05._24
 {
@@ -18,12 +19,28 @@ namespace _20._05._24
             //point.Print();
             //point2.Print();
             //point3.Print();
-            Triangle<int> triangle = new Triangle<int>(1,2,3);
-            Triangle<uint> triangle1 = new Triangle<uint>(5u,6u,7u);
-            Triangle<double> triangle2 = new Triangle<double>(4.5,5.5,6.5);
-            triangle.Print();
-            triangle1.Print();
-            triangle2.Print();
+
+            //Triangle<int> triangle = new Triangle<int>(1,2,3);
+            //Triangle<uint> triangle1 = new Triangle<uint>(5u,6u,7u);
+            //Triangle<double> triangle2 = new Triangle<double>(4.5,5.5,6.5);
+            //triangle.Print();
+            //triangle1.Print();
+            //triangle2.Print();
+            List<List<dynamic>> dynamics = new List<List<dynamic>>();
+            Segment<dynamic> segment;
+            for (int i = 0; i < 2; i++)
+            {
+                dynamics.Add(new List<dynamic>());
+                Console.Clear();
+                Console.WriteLine($"Введите 1 координату {i + 1} точки: ");
+                double x = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine($"Введите 2 координату {i + 1} точки: ");
+                double y = Convert.ToDouble(Console.ReadLine());
+                dynamics[i].Add(x);
+                dynamics[i].Add(y);
+            }
+            segment = new Segment<dynamic>(dynamics);
+            segment.Print();
             Console.Read();
         }
     }
---
+++ 20.05.24/Segment.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20._05._24
+{
+    class Segment <A>
+    {
+        private List<List<A>> segments;
+
+        public Segment() 
+        {
+
+        }
+        public Segment(List<List<A>> segments) 
+        {
+            this.segments = segments;
+        }
+
+        public List<List<A>> Segments { get => segments; set => segments = value; }
+
+        public double Lenght()
+        {
+            dynamic x1 = segments[0][0];
+            dynamic x2 = segments[1][0];
+            dynamic y1 = segments[0][1];
+            dynamic y2 = segments[1][1];
+            double l = Math.Sqrt(Math.Pow((double)x1 - (double)x2, 2) + Math.Pow((double)y1 - (double)y2, 2));
+            return l;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Координаты точек: {segments[0][0]},{segments[0][1]};{segments[1][0]},{segments[1][1]}\nДлина: {Lenght()}");
+        }
+    }
+}
